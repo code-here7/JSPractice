4e32ddf1c76fab8ff37a291f030a33701e680bfb
code-here7 <84375783+code-here7@users.noreply.github.com>
2025-04-10 11:07:46 +0530

    Create ConvertNormalToArrow.js

---
+++ ConvertNormalToArrow.js
@@ -0,0 +1,11 @@
+// Original
+// function greet(name) {
+//   return "Hello, " + name + "!";
+// }
+
+// Convert it to arrow function
+const greet = (name) => {
+    return "Hello, " + name + "!";
+}
+
+console.log(greet("Rima"));
